f48cc7fee8da1cd90cc82d61456854d0f4824126
ducutc94 <ducutc94@gmail.com>
2023-03-09 10:25:51 +0700

    ungdungmaytinh

--- 803/tiendola.js
+++ 803/tiendola.js
@@ -1,18 +1,11 @@
 function Tien() {
-    let amount = document.getElementById("amount").value;
-    let from = document.getElementById("from").value;
-    let to = document.getElementById("to").value;
-    let Tien;
-    console.log(from);
-    console.log(to);
-    if (from == "vn" && to == "usd") {
-        Tien = "Tien" + (amount / 23000) + "$";
-    } else if (from == "usd" && to=="vn") {
-        Tien = "Tien" + (amount * 23000) + "D";
-    } else if (from == "vn") {
-        Tien = "Tien" + amount + "D";
-    } else {
-        Tien = "Tien" + amount + "$";
-    }
-   document.getElementById("Tien").innerHTML=Tien;
+    let amount = +document.getElementById("amount").value;
+    let from = +document.getElementById("from").value;
+    let to = +document.getElementById("to").value;
+    let  Tien = (amount*to/from);
+    console.log(from)
+    console.log(to)
+    console.log(amount)
+    console.log(Tien)
+    alert("Tien" + Tien);
 }
--- 803/ungdung.js
+++ 803/ungdung.js
@@ -1,33 +1,52 @@
-function A() {
-    let nhap1 = document.getElementById("nhap1").value;
-    let nhap2 = document.getElementById("nhap2").value;
-    nhap1=parseInt(nhap1)
-    nhap2=parseInt(nhap2)
-    let a= nhap1 + nhap2;
-    document.getElementById("RD").innerHTML="ketqu" + a;
-}
-function S() {
-    let nhap1 = document.getElementById("nhap1").value;
-    let nhap2 = document.getElementById("nhap2").value;
-    nhap1=parseInt(nhap1)
-    nhap2=parseInt(nhap2)
-    let b= nhap1-nhap2;
-    document.getElementById("RD").innerHTML="ketqua" + b;
-}
-function M() {
-    let nhap1 = document.getElementById("nhap1").value;
-    let nhap2 = document.getElementById("nhap2").value;
-    nhap1=parseInt(nhap1)
-    nhap2=parseInt(nhap2)
-    let c=nhap1*nhap2;
-    document.getElementById("RD").innerHTML="ketqua" +c;
-}
-function D() {
-    let nhap1 = document.getElementById("nhap1").value;
-    let nhap2 = document.getElementById("nhap2").value;
-    nhap1=parseInt(nhap1)
-    nhap2=parseInt(nhap2)
-    let d= nhap1/nhap2;
-    document.getElementById("RD").innerHTML="ketqua" +d;
+// function A() {
+//     let nhap1 = document.getElementById("nhap1").value;
+//     let nhap2 = document.getElementById("nhap2").value;
+//     nhap1=parseInt(nhap1)
+//     nhap2=parseInt(nhap2)
+//     let a= nhap1 + nhap2;
+//     document.getElementById("RD").innerHTML="ketqu" + a;
+// }
+// function S() {
+//     let nhap1 = document.getElementById("nhap1").value;
+//     let nhap2 = document.getElementById("nhap2").value;
+//     nhap1=parseInt(nhap1)
+//     nhap2=parseInt(nhap2)
+//     let b= nhap1-nhap2;
+//     document.getElementById("RD").innerHTML="ketqua" + b;
+// }
+// function M() {
+//     let nhap1 = document.getElementById("nhap1").value;
+//     let nhap2 = document.getElementById("nhap2").value;
+//     nhap1=parseInt(nhap1)
+//     nhap2=parseInt(nhap2)
+//     let c=nhap1*nhap2;
+//     document.getElementById("RD").innerHTML="ketqua" +c;
+// }
+// function D() {
+//     let nhap1 = document.getElementById("nhap1").value;
+//     let nhap2 = document.getElementById("nhap2").value;
+//     nhap1=parseInt(nhap1)
+//     nhap2=parseInt(nhap2)
+//     let d= nhap1/nhap2;
+//     document.getElementById("RD").innerHTML="ketqua" +d;
+//
+// }
+
+function Tinh(dau) {
+    let nhap1 = +document.getElementById("nhap1").value;
+    let nhap2 = +document.getElementById("nhap2").value;
+    if (dau == "+") {
+        let c = nhap1 + nhap2;
+        document.getElementById("RD").innerHTML = "Ketqua" + c;
+    } else if (dau == "-") {
+        let d = nhap1 - nhap2;
+        document.getElementById("RD").innerHTML = "Ketqua" + d;
+    } else if (dau == "*") {
+        let e = nhap1 * nhap2;
+        document.getElementById("RD").innerHTML = "ketqua" + e;
+    } else if (dau == "/") {
+        let d = nhap1 / nhap2;
+        document.getElementById("RD").innerHTML = "ketqua" + d;
+    }
 
 }
